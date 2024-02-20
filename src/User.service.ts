a6f74dd318b1c9dd2c01adc98c522b9fa19186cb
Ashis Kumar Pradhan <38760485+akp111@users.noreply.github.com>
2024-02-20 17:48:48 +0000

    completed assignment

--- src/User.service.ts
+++ src/User.service.ts
@@ -2,4 +2,9 @@ import { UserRequestDto } from './Database'
 import { UserRepository } from './User.repository'
 
 // NOTE: Make userRepo public and not private. This is required for the tests!
-export class UserService {}
+export class UserService {
+    public readonly userRepo = new UserRepository();
+    createUser(user: UserRequestDto){
+        return this.userRepo.createUser(user);
+    }
+}
